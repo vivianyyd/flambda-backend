83b10e8a80a8cbbc12adcf299e7049e4fd43f859
Vivian <vivianyyd@gmail.com>
2023-07-31 12:49:13 -0400

    Add test

--- ocaml/testsuite/tests/typing-implicit-source-positions/implicit_argument.ml
+++ ocaml/testsuite/tests/typing-implicit-source-positions/implicit_argument.ml
@@ -4,11 +4,21 @@
 
 let f = fun ~(src_pos:[%src_pos]) () -> src_pos
 [%%expect{|
-val f : src_pos:lexing_position -> unit -> lexing_position = <fun>
+val f : src_pos:[%src_pos] -> unit -> lexing_position = <fun>
 |}]
 
 let _ = f ();;
 [%%expect{|
 - : lexing_position =
-{pos_fname = ""; pos_lnum = 1; pos_bol = 156; pos_cnum = 164}
+{pos_fname = ""; pos_lnum = 1; pos_bol = 151; pos_cnum = 159}
+|}]
+
+let g = fun ~(a:[%src_pos]) ?(c = 0) ~(b:[%src_pos]) () -> ()
+[%%expect{|
+val g : a:[%src_pos] -> ?c:int -> b:[%src_pos] -> unit -> unit = <fun>
+|}]
+
+let _ = g ~a:Lexing.dummy_pos () ;;
+[%%expect{|
+- : unit = ()
 |}]
